305e0d6ab2985767793c01132a2c34b9af3c6069
Niterich <52295078+Niterich@users.noreply.github.com>
2020-03-07 13:43:26 -0700

    started working on seeding database

--- db/seeds.js
+++ db/seeds.js
@@ -5,16 +5,22 @@ const insert = (value, val, valyou, valll) => {
         homeTown: valyou,
         active: valll
     }).then(foo => {
-        console.log("Artist Added!")
+        console.log("Artist Added!");
     });
-  };
+};
+
 //Rock
-  insert('The Beatles', 'Rock', 'Liverpool', false);
+insert("The Beatles", "Rock", "Liverpool", false);
+insert("The Rolling Stones", "Rock", "London, England", true);
 //Hip Hop
-  insert('Nas', 'Hip Hop', 'New York', true);
+insert("Nas", "Hip Hop", "New York", true);
+insert("The Wu-Tang Clan", "Hip Hop", "New York, New York", true);
 //Pop
-  insert('Ariana Grande', 'Pop', 'Florida', true)
+insert("Ariana Grande", "Pop", "Florida", true);
+insert("Kali Uchis", "Pop", "Pereira, Risaralda, Columbia", true);
 //Electronic
-  insert('Deadmau5', "Electronic", "Canada", true);
+insert("Deadmau5", "Electronic", "Canada", true);
+insert("Dan Deacon", "Electronic", "Baltimore, Maryland", true);
 //Alternative
-  insert('Nirvana', 'Alternative', 'Seattle', false);
+insert("Nirvana", "Alternative", "Seattle", false);
+insert("Sonic Youth", "Alternative", "New York, New York", false);
