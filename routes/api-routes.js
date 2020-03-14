8d5abcec85027df15dca1c0e450c355f0ea327e3
Evan Pylar <evan.m.pylar@gmail.com>
2020-03-14 06:32:25 -0700

    syntax ajax

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -63,7 +63,7 @@ app.post("/api/posts", function(req, res) {
       comment: req.body.body
       
     }).then(function(dbPost) {
-        console.log(dbPost);
+        // console.log(dbPost);
         res.json(dbPost);
       });
   });
